6753cc127d0bc278a7524b9203e97f187f1e0f87
microlinklu <luwenmail1394@163.com>
2018-08-23 09:28:12 +0800

    VRGamedemo
    proj180823

---
+++ demo1/release/wxgame/js/LayaSample.js
@@ -0,0 +1,16 @@
+//Laya.init(600, 400);
+
+Laya3D.init(1733,768,false);
+var scene=new Laya.VRScene();
+Laya.stage.addChild(scene);
+// var sprite=new Laya.Sprite3D();
+// sprite.loadHierarchy("");
+// scene.addChild(sprite);
+var camera=new Laya.VRCamera();
+scene.addChild(camera);
+var boxGeo=new Laya.BoxMesh(1,1,1)
+var box=new Laya.MeshSprite3D(boxGeo);
+box.transform
+scene.addChild(box);
+var light=new Laya.DirectionLight();
+scene.addChild(light)
